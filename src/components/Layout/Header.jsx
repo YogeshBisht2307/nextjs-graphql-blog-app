33c4830334eef0e8621b983f2fe8db4ac2540c39
Yogesh Bisht <yogeshbisht.2307@gmail.com>
2022-02-06 03:37:19 +0530

    color fixes on header" ;

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -14,14 +14,14 @@ const Header = () => {
 						<div>
 							<a href="/" className="flex items-center py-4 px-2">
 								<img src="/favicon.png" alt="Logo" className="h-8 w-8 lg:h-12 lg:w-12 mr-1"/>
-								<h2 className="font-semibold text-gray-200 text-xl lg:text-2xl md:text-2xl">{ BRAND_NAME }</h2>
+								<h2 className="font-semibold text-gray-100 text-xl lg:text-2xl md:text-2xl">{ BRAND_NAME }</h2>
 							</a>
 						</div>
 					
 						<div className="hidden md:flex items-center space-x-1 ml-4">
-							<a href="/about" className="py-4 px-2 text-gray-200 font-medium transition duration-300">About</a>
-                            <a href="/services" className="py-4 px-2 text-gray-200 font-medium transition duration-300">Services</a>
-							<a href="/contact" className="py-4 px-2 text-gray-200 font-medium transition duration-300">Contact Us</a>
+							<a href="/about" className="py-4 px-2 text-gray-100 font-medium transition duration-300">About</a>
+                            <a href="/services" className="py-4 px-2 text-gray-100 font-medium transition duration-300">Services</a>
+							<a href="/contact" className="py-4 px-2 text-gray-100 font-medium transition duration-300">Contact Us</a>
 						</div>
 					</div>
 				
